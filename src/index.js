46b2f77a62713f1b9fd04025523651705986f438
ManuDev03 <manuprajan03@gmail.com>
2021-05-11 17:55:57 +0530

    middleware for site under maintance mode

--- src/index.js
+++ src/index.js
@@ -6,9 +6,26 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+   
+
+// })
+
+// middleware for maintains mode 
+// 1. register a new middleware function
+// 2. send back a maintance message with 503 status code
+// 3. try your request from server and confirm status 
+
+app.use((req, res, next) => {
+    res.status(503).send('Site is currently down check back soon!')
+})
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
+
+// without middleware: new request --> run route handler
+// with middleware: new request --> do something --> run route handler
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
